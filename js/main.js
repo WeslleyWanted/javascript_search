0f97a1072f4daa5d314168f59a5ca3909b2a48c9
Weslley Oliveira <weslleyoliveirawanted@gmail.com>
2020-07-26 13:37:46 -0400

    criando funcões de busca em js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function searchBoletim(){
+    var phrase = prompt("Buscar boletim");
+    //alert("funfa");
+    console.log(phrase);
+}
+function setgg(){
+    console.log(document.getElementsByName(phrase));
+}
